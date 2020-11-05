b99f7423bdfd13ec13e611542e4d5afba01f6f61
John Car Lo <33212010+firemax13@users.noreply.github.com>
2020-11-05 14:59:36 +0800

    Create when.c

---
+++ when.c
@@ -0,0 +1,61 @@
+/*
+ *  When Module for When Company
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ */
+ 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+ 
+#ifdef CONFIG_ASK_WHEN
+#include "when.h
+#endif
+ 
+/*
+ * Include CPP for Other when functions
+ */
+#include "when.cpp"
+#include "LICENSE"
+ 
+/*
+ * Define Same Readings
+ */
+#define wen when
+#define WEN WHEN
+#define BAN /ban
+
+
+static int ask_when = warn_will * 1;
+static struct wEn *when;
+static struct Wen *when;
+static struct weN *when;
+static struct WEN *when;
+
+static int people_ask_when(struct warn *warn, struct ban *ban,
+                              struct will *will)
+{
+	  people->ask_when = will->warn;
+	  return 0;
+    people->ask_when_no-stop = will->warn-multiple;
+    return 1;
+    people->ask_when-more = will->ban;
+    go to BAN;
+}
+
+#ifdef return 1
+static inline void people_ask_when-more(struct ban *ban, will *will)
+{
+    if (when-more) {
+            BAN(people_who_ask_when-more: @id: when-more%);
+    }
+    exit 1;
+}
+#endif
+ 
